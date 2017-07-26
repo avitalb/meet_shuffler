8b75dcf095ae3446e97f308e379a96a659f517ad
Avital Baral <abaral@mit.edu>
2017-07-26 12:41:29 +0300

    cleaned up user script

--- final_script.py
+++ final_script.py
@@ -1,20 +1,18 @@
 from section_maker import *
 
 input_section = ""
-while input != "Q":
-    input_section = input("Which section would you like to shuffle (A,B,or C)? Press Q to quit: ")
-    if input_section == "A":
-        sectiona.shuffle()
-        print(sectiona)
-    elif input_section == "B":
-        sectionb.shuffle()
-        print(sectionb)
-    elif input_section == "C":
-        sectionc.shuffle()
-        print(sectionc)
-    elif input_section == "Q":
-        break
-    else:
-        print("You entered an invalid input!")
+
+input_section = input("Which section would you like to shuffle (A, B, or C)? ")
+if input_section == "A":
+    sectiona.shuffle()
+    print(sectiona)
+elif input_section == "B":
+    sectionb.shuffle()
+    print(sectionb)
+elif input_section == "C":
+    sectionc.shuffle()
+    print(sectionc)
+else:
+    print("You entered an invalid input!")
 
         
