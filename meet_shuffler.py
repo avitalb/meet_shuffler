23486ee3c20c9f661fa6e8f0005d5222d32bb3d3
Avital Baral <abaral@mit.edu>
2017-07-26 09:41:24 +0300

    initial mixing algorithm

--- meet_shuffler.py
+++ meet_shuffler.py
@@ -24,5 +24,46 @@ class Section():
         for stud0,stud1 in self.pairs:
             to_return += "["+repr(stud0)+" and "+repr(stud1)+"]\n"
         return to_return
-    
+    #helper to update mixing checks
+    def update_checks(self):
+        #initializing
+        keys = list(self.students.keys())
+        gender = self.students[keys[0]].gender
+        nat = self.students[keys[0]].nat
+        for i in range(1,len(self.students)):
+            #nat check
+            if self.students[keys[i]].nat == nat:
+                self.nat_check = True
+            if self.students[keys[i]].nat == gender:
+                self.gender_check = True
+
+    def shuffle(self):
+        #base cases
+        #also checks for uneven numbers
+        if len(self.students) == 1:
+            self.pairs.append((self.students[0],None))
+            return
+        elif len(self.students) == 0:
+            return
+        #find 2 different students
+        i0 = random.choice(list(self.students.keys()))
+        i1 = random.choice(list(self.students.keys()))
+        while i0 == i1:
+            i0 = random.choice(list(self.students.keys()))
+            i1 = random.choice(list(self.students.keys()))
+
+        student0 = self.students[i0]
+        student1 = self.students[i1]  
+
+        #gender and nationality check
+        self.update_checks()
+
+        if student0.nat != student1.nat and student0.gender != student1.gender or student0.nat != student1.nat and self.gender_check \
+        or student0.gender != student1.gender and self.nat_check or self.gender_check and self.nat_check:
+            self.pairs.append((student0,student1))
+            del self.students[i0]
+            del self.students[i1]
+
+        #recursive step
+        self.shuffle()
     
